57e1574b000225442f9007e533c9b26244ea5b6e
Dipoolx2 <zygrobusiness@gmail.com>
2025-08-12 21:24:49 +0200

    Initial determinization

--- src/FSM/NFADeterminizer.cpp
+++ src/FSM/NFADeterminizer.cpp
@@ -1,8 +1,15 @@
 #include "FSM/NFADeterminizer.h"
-#include <iostream>
-
-
+#include "FSM/FSM.h"
+#include <unordered_set>
 
 void NFADeterminizer::determinize(NFA& nfa) const {
-    std::cout << "Test - determinizing NFA!\n";
+    std::unordered_set<char>& alphabet = nfa.alphabet;
+
+    NFAStatePtr initial = std::make_shared<NFAState>(false);
+    NFAStatePtr sink = std::make_shared<NFAState>(false);
+
+    // Creating complete transition for sink state
+    for (char c : alphabet) {
+        sink->transitions[c].push_back(sink);
+    }
 }
