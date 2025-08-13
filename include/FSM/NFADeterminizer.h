4fc47c0f3cc645e422c9b62b7d9a068bc04dc7a6
Dipoolx2 <zygrobusiness@gmail.com>
2025-08-13 19:55:44 +0200

    backup

--- include/FSM/NFADeterminizer.h
+++ include/FSM/NFADeterminizer.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include "FSM/FSM.h"
+#include <memory>
+#include <unordered_set>
+#include <utility>
 
-using NFAStatePtr = std::shared_ptr<NFAState>;
+using StatePtr = std::shared_ptr<NFAState>;
+using StatePtrSet = std::unordered_set<StatePtr>;
+using StateSetPair = std::pair<StatePtr, StatePtrSet>;
 
 class NFADeterminizer {
     public:
     virtual ~NFADeterminizer() = default;
 
     void determinize(NFA& nfa) const;
+    StatePtr makeSinkState(std::unordered_set<char>& alphabet) const;
 };
--- src/FSM/NFADeterminizer.cpp
+++ src/FSM/NFADeterminizer.cpp
@@ -1,15 +1,36 @@
 #include "FSM/NFADeterminizer.h"
 #include "FSM/FSM.h"
+#include <memory>
+#include <queue>
 #include <unordered_set>
 
-void NFADeterminizer::determinize(NFA& nfa) const {
-    std::unordered_set<char>& alphabet = nfa.alphabet;
-
-    NFAStatePtr initial = std::make_shared<NFAState>(false);
-    NFAStatePtr sink = std::make_shared<NFAState>(false);
+StatePtr NFADeterminizer::makeSinkState(std::unordered_set<char>& alphabet) const {
+    StatePtr sink = std::make_shared<NFAState>(false);
 
     // Creating complete transition for sink state
     for (char c : alphabet) {
         sink->transitions[c].push_back(sink);
     }
+
+    return sink;
+}
+
+void NFADeterminizer::determinize(NFA& nfa) const {
+    std::unordered_set<char>& alphabet = nfa.alphabet;
+
+    StatePtr initial = std::make_shared<NFAState>(false);
+    StatePtr sink = makeSinkState(alphabet);
+
+    //std::unordered_set<StatePtrSet> visited;
+    // TODO: CREATE A HASHABLE SUBSET WRAPPER
+    // -- FOR SMALL SIZES USE BITWISE OPS
+    // -- FOR LARGER SIZES USE SORTED ARRAYS
+    std::queue<StateSetPair> q;
+
+    q.push({initial, {nfa.start}});
+
+    while (!q.empty()) {
+
+    }
+
 }
